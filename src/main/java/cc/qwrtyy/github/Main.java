abc2343827541716fb5943b92a0bf7e30744995e
qwrtyy <topanaprzypale@gmail.com>
2022-12-01 15:52:26 +0100

    .

--- src/main/java/cc/qwrtyy/github/Main.java
+++ src/main/java/cc/qwrtyy/github/Main.java
@@ -2,16 +2,12 @@ package cc.qwrtyy.github;
 
 import cc.qwrtyy.github.handlers.ServerListPingHandler;
 import org.bukkit.plugin.java.JavaPlugin;
-import static pl.qwrtyy.tools.utils.listener.ListenerReg.registerListener;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        registerListeners();
-    }
-    public void registerListeners(){
-        registerListener(this, new ServerListPingHandler());
+        getServer().getPluginManager().registerEvents(new ServerListPingHandler(), this);
     }
 
     @Override
--- src/main/java/cc/qwrtyy/github/handlers/ServerListPingHandler.java
+++ src/main/java/cc/qwrtyy/github/handlers/ServerListPingHandler.java
@@ -1,20 +1,43 @@
 package cc.qwrtyy.github.handlers;
 
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
-import pl.qwrtyy.tools.utils.Utils;
 
 public class ServerListPingHandler implements Listener {
     @EventHandler
     public void onSeverListPing(final ServerListPingEvent event) {
-        event.setMotd(Utils.fixColor("&d&l◈ &dKCode&7.&fsite &8>> &7Officjalny serwer &dKCode!")
+        event.setMotd(fixColor("&d&l◈ &dKCode&7.&fsite &8>> &7Officjalny serwer &dKCode!")
                 + "\n" +
-                Utils.fixColor("&d&l◈ &7To tutaj powstaly wszystkie &dprojekty!"));
+                fixColor("&d&l◈ &7To tutaj powstaly wszystkie &dprojekty!"));
 
     }
     @EventHandler
     public void onMaxPlayers(final ServerListPingEvent event) {
-        event.setMaxPlayers(event.getNumPlayers() + 69);
+        event.setMaxPlayers(event.getNumPlayers() + 100);
+    }
+    public static String fixColor(final String s) {
+        return ChatColor.translateAlternateColorCodes('&', s)
+                .replace(">>", "»")
+                .replace("<<", "«")
+                .replace("((*))", "ó")
+                .replace("((o))", "ó")
+                .replace("<>", "✖")
+                .replace("((n))", "ń")
+                .replace("((l))", "ł")
+                .replace("((s))", "ś")
+                .replace("((a))", "ą")
+                .replace("<SERCE>", "❤")
+                .replace("((n))", "ń")
+                .replace("((c))", "ć")
+                .replace("MYSZKA", "▄")
+                .replace("{PI}", "π")
+                .replace("{SZP}", "→")
+                .replace("{SZL}", "←")
+                .replace("<X>", "✘")
+                .replace("<Y>", "✔")
+                .replace("{UNDERLINE}", "۔")
+                .replace("{KROPKA}", "●");
     }
 }
